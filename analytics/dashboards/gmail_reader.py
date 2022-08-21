c1dbd38deb6617a54b7662023f076896d7949088
paishrikrishna <2017.shrikrishna.pai@ves.ac.in>
2022-08-21 23:14:20 +0530

    file_path_added

--- analytics/dashboards/gmail_reader.py
+++ analytics/dashboards/gmail_reader.py
@@ -18,18 +18,18 @@ def credit_card_emails(firebase_obj):
         existing_data.append(doc.to_dict())
         
     creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('/home/shripais003/personal_analysis/analytics/dashboards/token.pickle'):
+        with open('/home/shripais003/personal_analysis/analytics/dashboards/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '/home/shripais003/personal_analysis/analytics/dashboards/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('/home/shripais003/personal_analysis/analytics/dashboards/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
